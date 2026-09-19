a86368716e8f05f601f92d8eac7fe89f8f1704f9
Void <152130497+i1void@users.noreply.github.com>
2026-09-20 04:24:38 +0700

    .

--- plugins/media/toimg.js
+++ plugins/media/toimg.js
@@ -1,3 +1,5 @@
+const { convertWebpToPng, isAnimatedWebp } = require('../../lib/convert')
+
 module.exports = {
   name: 'toimg',
   aliases: [],
@@ -8,7 +10,9 @@ module.exports = {
     if (!m.quoted || m.quoted.mtype !== 'stickerMessage') return reply(`Reply to a sticker with the caption ${prefix}toimg`)
     try {
       const buffer = await sock.downloadMediaMessage(m.quoted)
-      await sock.sendMessage(m.chat, { image: buffer }, { quoted: m })
+      if (isAnimatedWebp(buffer)) return reply("Animated stickers can't be converted to an image.")
+      const image = await convertWebpToPng(buffer)
+      await sock.sendMessage(m.chat, { image }, { quoted: m })
     } catch (err) {
       console.error(err)
       reply(config.messages.error)
